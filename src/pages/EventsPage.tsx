e444d73fb6303e84194c1604ba96f01d173bae4d
joaoPedro_tielletDem <JoaoPedro.TielletDemari@dellteam.com>
2024-08-31 07:06:25 -0300

    layout of the events page

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import CardComponent from "../components/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../components/ui/carousel";
+import { Card, CardContent } from "../components/ui/card";
 
 type Props = {};
 
@@ -7,10 +15,74 @@ const EventsPage = (props: Props) => {
   return (
     <main className="px-4 gap-4 flex flex-col mb-24">
       <h1 className="text-3xl font-bold">Eventos Disponíveis</h1>
-      <CardComponent></CardComponent>
-      <CardComponent></CardComponent>
-      <CardComponent></CardComponent>
-      <CardComponent></CardComponent>
+      <h2 className="text-xl font-semibold">Treinamento</h2>
+      <Carousel className="w-full max-w-sm">
+        <CarouselContent>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
+      <h2 className="text-xl font-semibold">Doações</h2>
+      <Carousel className="w-full max-w-sm">
+        <CarouselContent>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
+      <h2 className="text-xl font-semibold">Simulações</h2>
+      <Carousel className="w-full max-w-sm">
+        <CarouselContent>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
+      <h2 className="text-xl font-semibold">Multirão</h2>
+      <Carousel className="w-full max-w-sm">
+        <CarouselContent>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+          <CarouselItem className="basis-3/5 lg:basis-1/3">
+            <CardComponent></CardComponent>
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
     </main>
   );
 };
