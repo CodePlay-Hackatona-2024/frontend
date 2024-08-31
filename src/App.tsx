88fa1d2858cf287976ee2cd018b79cf0bfdd4f1f
joaoPedro_tielletDem <JoaoPedro.TielletDemari@dellteam.com>
2024-08-31 05:08:36 -0300

    start of the footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToggleGroupDemo } from "./components/navigationBar";
 
 export default function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <div className="relative min-h-screen md:block">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div></div>} />
+            <Route path="/login" element={<div></div>} />
+            <Route path="/register" element={<div></div>} />
+            <Route path="/events" element={<div></div>} />
+            <Route path="/my-events" element={<div></div>} />
+            <Route path="/shop" element={<div></div>} />
+            <Route path="/coupons" element={<div></div>} />
+          </Routes>
+        </BrowserRouter>
+        <ToggleGroupDemo />
       </div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<div></div>} />
-          <Route path="/login" element={<div></div>} />
-          <Route path="/register" element={<div></div>} />
-          <Route path="/events" element={<div></div>} />
-          <Route path="/my-events" element={<div></div>} />
-          <Route path="/shop" element={<div></div>} />
-          <Route path="/coupons" element={<div></div>} />
-        </Routes>
-      </BrowserRouter>
     </>
   );
 }
