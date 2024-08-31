0c7ad00d7343cc135d1fbc6adfa8fc0e72beaf42
Guilherme Kuhn <guilherme.kuhn002@edu.pucrs.br>
2024-08-31 03:39:58 -0300

    Added routes to App.tsx and changed package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div></div>} />
+        <Route path="/login" element={<div></div>} />
+        <Route path="/register" element={<div></div>} />
+        <Route path="/events" element={<div></div>} />
+        <Route path="/my-events" element={<div></div>} />
+        <Route path="/shop" element={<div></div>} />
+        <Route path="/coupons" element={<div></div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
