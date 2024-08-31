e29d4c93140da4c4920e97597ebb76f0b1f5ec6e
gabrielpetry23 <gabriel.petry23@gmail.com>
2024-08-31 05:12:47 -0300

    fix conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FirstPage from "./pages/FirstPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import EventsPage from "./pages/EventsPage";
-import MyEventsPage from "./pages/MyEventsPage";
-import ShopPage from "./pages/ShopPage";
-import CouponsPage from "./pages/CouponsPage";
+// import EventsPage from "./pages/EventsPage";
+// import MyEventsPage from "./pages/MyEventsPage";
+// import ShopPage from "./pages/ShopPage";
+// import CouponsPage from "./pages/CouponsPage";
 
 export default function App() {
   return (
@@ -14,10 +14,10 @@ export default function App() {
           <Route path="/" element={<FirstPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/events" element={<EventsPage />} />
+          {/* <Route path="/events" element={<EventsPage />} />
           <Route path="/my-events" element={<MyEventsPage />} />
           <Route path="/shop" element={<ShopPage />} />
-          <Route path="/coupons" element={<CouponsPage />} />
+          <Route path="/coupons" element={<CouponsPage />} /> */}
         </Routes>
       </BrowserRouter>
   );
