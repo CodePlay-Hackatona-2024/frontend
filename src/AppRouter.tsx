1c28262481184b648a1535631b97cdeafc83479a
joaoPedro_tielletDem <JoaoPedro.TielletDemari@dellteam.com>
2024-08-31 15:56:09 -0300

    everything

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 import FirstPage from "./pages/FirstPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -8,8 +14,9 @@ import EventsPage from "./pages/EventsPage";
 import { Header } from "./components/header/Header";
 import MyEventsPage from "./pages/MyEventsPage";
 import ShopPage from "./pages/ShopPage";
-import { AuthProvider, useAuth } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 import EventInfoPage from "./pages/EventInfoPage";
+import CouponsPage from "./pages/CouponsPage";
 
 const AppRouter: React.FC = () => {
   return (
@@ -24,7 +31,7 @@ const AppRouter: React.FC = () => {
           <Route element={<PrivateWrapper />}>
             <Route path="/events" element={<EventsPage />} />
             <Route path="/my-events" element={<MyEventsPage />} />
-            <Route path="/rewards" element={<div />} />
+            <Route path="/rewards" element={<CouponsPage />} />
             <Route path="/shop" element={<ShopPage />} />
             <Route path="/event-info/:id" element={<EventInfoPage />} />
           </Route>
@@ -35,24 +42,17 @@ const AppRouter: React.FC = () => {
 };
 
 const PrivateWrapper = () => {
-  // const { isAuthenticated } = useAuth();
-  const isAuthenticated = true;
-
-  return isAuthenticated ? (
+  return (
     <>
       <Header />
       <Outlet />
       <ToggleGroupDemo />
     </>
-  ) : (
-    <Navigate to="/login" />
   );
 };
 
 const PublicWrapper = () => {
-  const { isAuthenticated } = useAuth();
-
-  return isAuthenticated ? <Navigate to="/events" /> : <Outlet />;
+  return <Outlet />;
 };
 
 export default AppRouter;
