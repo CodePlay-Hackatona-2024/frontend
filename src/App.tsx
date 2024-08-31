6ca72a3cfcb9d2e8f73df14e76cfac4e3214db9d
gabrielpetry23 <gabriel.petry23@gmail.com>
2024-08-31 04:22:45 -0300

    rascunho FirstPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FirstPage from "./pages/FirstPage";
 
 export default function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-      </div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div></div>} />
+          <Route path="/" element={<FirstPage />} />
           <Route path="/login" element={<div></div>} />
           <Route path="/register" element={<div></div>} />
           <Route path="/events" element={<div></div>} />
@@ -17,6 +14,5 @@ export default function App() {
           <Route path="/coupons" element={<div></div>} />
         </Routes>
       </BrowserRouter>
-    </>
   );
 }
